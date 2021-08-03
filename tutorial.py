6dcad204685f8c54d8ace4158bc1f7bb1678a5d2
Bazzan <bazzanapalowo@gmail.com>
2021-08-03 17:06:30 +0100

    updated-car-model

--- tutorial.py
+++ tutorial.py
@@ -1,14 +1,18 @@
 
 class Car:
-    def __init__(self, model):
+    def __init__(self, model, color):
         self.model = model
+        self.color = color
 
     def drive(self):
-        print("I'm driving")
+        print(f"I'm driving a {self.color} {self.model} car")
 
     def stop(self):
         print('Stop')
 
 
-toyota = Car('Toyota')
+toyota = Car('Toyota', 'red')
+hyundia = Car('Hynudia', 'purple')
+
 toyota.drive()
+hyundia.drive()
