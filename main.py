58160476abfcaa72234373348b7393068245f5ad
Bazzan <bazzanapalowo@gmail.com>
2021-08-12 14:06:03 +0100

    oo_design

---
+++ main.py
@@ -0,0 +1,5 @@
+
+from tutorial import ATM
+
+bazzan = ATM(12338484)
+print(bazzan._bank)
--- tutorial.py
+++ tutorial.py
@@ -1,18 +1,43 @@
 
-class Car:
-    def __init__(self, model, color):
-        self.model = model
-        self.color = color
-
-    def drive(self):
-        print(f"I'm driving a {self.color} {self.model} car")
-
-    def stop(self):
-        print('Stop')
+import random
 
 
-toyota = Car('Toyota', 'red')
-hyundia = Car('Hynudia', 'purple')
+class ATM:
 
-toyota.drive()
-hyundia.drive()
+    bank = 'First Bank ATM'
+
+    def __init__(self, number):  # initializer, constructors.
+        self.number = number
+        self.account_balance = random.randint(1000, 2000)
+
+    def withdraw(self, amount):
+        print(f"{amount} withdrawn from account number {self.number}")
+
+    def transfer(self, amount):
+        print(f"{amount} transferred from account number {self.number}")
+
+    def check_balance(self):
+        print(f"Bank Name: {self.bank} :::::: Account Balance: {self.account_balance}")
+
+    def __repr__(self):
+        return f"This object belongs to the person with :: {self.number}"
+
+
+bazzan = ATM(3114233162)
+
+
+kunle = ATM(3112012604)
+
+
+
+# class Die:
+#     # """Simulate a 6-sided die."""
+#
+#     def roll(self):
+#         self.value = random.randint(1, 6)
+#         return self.value
+#
+#     def getValue(self):
+#         return self.value
+#
+# print(help(Die))
